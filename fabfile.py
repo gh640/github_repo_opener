e92ed7f03d93b5401b3ecfd66ce1b238411d03cb
Goto Hayato <habita.gh@gmail.com>
2018-03-02 15:56:13 +0900

    Issue #5: Add `upload_to_pypi` command in `fabfile.py`.

--- fabfile.py
+++ fabfile.py
@@ -26,3 +26,11 @@ def build():
 def clear_build():
     '''Clears files generated.'''
     local('rm -r dist build github_repo_opener.egg-info')
+
+
+def upload_to_pypi(env='test'):
+    '''Uploads the dist files to pypi.'''
+    if env == 'prod':
+        local('twine upload dist/*')
+    else:
+        local('twine upload --repository testpypi dist/*')
