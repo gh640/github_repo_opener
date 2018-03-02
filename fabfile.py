200ec5a9950168b8e857b208d410b6dbf54f6010
Goto Hayato <habita.gh@gmail.com>
2018-03-02 14:29:26 +0900

    Issue #3: Add `fabfile.py`.

---
+++ fabfile.py
@@ -0,0 +1,28 @@
+from fabric.api import local
+
+
+CMD_PREFIX = 'pipenv run '
+
+
+def validate():
+    '''Validates files.'''
+    _validate_rst()
+    _validate_manifest()
+
+
+def _validate_rst():
+    local(CMD_PREFIX + 'rstcheck README.rst')
+
+
+def _validate_manifest():
+    local(CMD_PREFIX + 'check-manifest')
+
+
+def build():
+    '''Builds package.'''
+    local(CMD_PREFIX + 'python setup.py sdist bdist_wheel')
+
+
+def clear_build():
+    '''Clears files generated.'''
+    local('rm -r dist build github_repo_opener.egg-info')
